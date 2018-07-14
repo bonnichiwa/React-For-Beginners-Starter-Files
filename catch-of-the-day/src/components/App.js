42d854d9556510cdc5489bb89686e34f3289faf5
bonnichiwa <bonnie.pham@ryerson.ca>
2018-07-14 09:50:11 -0400

    added components and methods

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Header from './Header';
+import Inventory from './Inventory';
+import Order from './Order';
+import Fish from './Fish';
+import fishes from '../sample-fishes';
+
+class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {}
+    };
+
+    addFish = (fish) => {
+        // 1. Take a copy of the existing state
+        const fishes = {...this.state.fishes};
+        // 2. Add our new fish to that fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+        // 3. Set the new fishes object to state
+        this.setState({fishes});
+    };
+
+    loadSampleFishes = () => {
+        this.setState({fishes});
+    };
+
+    render() {
+        return (
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    <Header tagline="Fresh Seafood Market"/>
+                    <ul className="fishes">
+                        { Object.keys(this.state.fishes).map(
+                            key => <Fish key={key} details={this.state.fishes[key]} />
+                            )
+                        }
+                    </ul>
+                </div>
+                <Order />
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
+            </div>
+        )
+    }
+}
+
+export default App;
